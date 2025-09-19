070c27dc6373e07fc31b050f4c3c436a2a00c0da
ABDULLAh <fahadkamran2103@gmail.com>
2025-09-19 02:54:26 -0700

    Create task3.cpp

---
+++ lab04/task3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void shellSort(int arr[], int size) {
+    for (int gap = size / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < size; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Original array:\n";
+    printArray(arr, n);
+
+    shellSort(arr, n);
+
+    cout << "Sorted array using Shell Sort:\n";
+    printArray(arr, n);
+
+    return 0;
+}
